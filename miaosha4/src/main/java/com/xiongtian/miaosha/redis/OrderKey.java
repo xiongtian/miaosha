3950dc8828356d2b7d07528cd4058778ec87fdff
xiongtian <764578422@qq.com>
2020-12-11 13:58:57 +0800

    将秒杀订单缓存到数据redis中

--- miaosha4/src/main/java/com/xiongtian/miaosha/redis/OrderKey.java
+++ miaosha4/src/main/java/com/xiongtian/miaosha/redis/OrderKey.java
@@ -7,7 +7,15 @@ package com.xiongtian.miaosha.redis;
  */
 
 public class OrderKey extends BasePrefix {
+
+    public OrderKey( String prefix) {
+        super( prefix);
+    }
+
+
     public OrderKey(int expireSeconds, String prefix) {
         super(expireSeconds, prefix);
     }
+
+    public static OrderKey getMiaoshaOrderByUidGid = new OrderKey("moug");
 }
--- miaosha4/src/main/java/com/xiongtian/miaosha/service/impl/OrderServiceIpml.java
+++ miaosha4/src/main/java/com/xiongtian/miaosha/service/impl/OrderServiceIpml.java
@@ -4,6 +4,8 @@ import com.xiongtian.miaosha.dao.OrderDao;
 import com.xiongtian.miaosha.domain.MiaoshaOrder;
 import com.xiongtian.miaosha.domain.MiaoshaUser;
 import com.xiongtian.miaosha.domain.OrderInfo;
+import com.xiongtian.miaosha.redis.OrderKey;
+import com.xiongtian.miaosha.redis.RedisService;
 import com.xiongtian.miaosha.service.OrderService;
 import com.xiongtian.miaosha.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,21 +26,27 @@ public class OrderServiceIpml implements OrderService {
     @Autowired
     OrderDao orderDao;
 
+    @Autowired
+    RedisService redisService;
+
+
     /**
      * 判断用户有没有秒杀的商品
+     *
      * @param userId
      * @param goodsId
      * @return
      */
     @Override
     public MiaoshaOrder getMiaoshaOrderByUserIdAndGoodsId(Long userId, long goodsId) {
-
-
-        return orderDao.getMiaoshaOrderByUserIdAndGoodsId(userId,goodsId);
+        //return orderDao.getMiaoshaOrderByUserIdAndGoodsId(userId,goodsId);
+        //不存数据库中查而是从缓存中获取
+        return redisService.get(OrderKey.getMiaoshaOrderByUidGid, "" + userId + "_" + goodsId, MiaoshaOrder.class);
     }
 
     /**
      * 生成订单
+     *
      * @param miaoshaUser
      * @param goods
      * @return
@@ -46,7 +54,7 @@ public class OrderServiceIpml implements OrderService {
     @Override
     @Transactional
     public OrderInfo createOrder(MiaoshaUser miaoshaUser, GoodsVo goods) {
-        OrderInfo  orderInfo = new OrderInfo();
+        OrderInfo orderInfo = new OrderInfo();
         orderInfo.setCreateDate(new Date());
         orderInfo.setDeliveryAddrId(0L);
         orderInfo.setGoodsCount(1);
@@ -56,12 +64,16 @@ public class OrderServiceIpml implements OrderService {
         orderInfo.setOrderChannel(1);
         orderInfo.setStatus(0);
         orderInfo.setUserId(miaoshaUser.getId());
-        long orderId=orderDao.insert(orderInfo);
-        MiaoshaOrder miaoshaOrder =  new MiaoshaOrder();
+        long orderId = orderDao.insert(orderInfo);
+        MiaoshaOrder miaoshaOrder = new MiaoshaOrder();
         miaoshaOrder.setGoodsId(goods.getId());
         miaoshaOrder.setOrderId(orderId);
         miaoshaOrder.setUserId(miaoshaUser.getId());
         orderDao.insertMiaoshaOrder(miaoshaOrder);
+
+        // 生存订单之后写入缓存
+        redisService.set(OrderKey.getMiaoshaOrderByUidGid, "" + miaoshaOrder.getId() + "_" + goods.getId(), miaoshaOrder);
+
         return orderInfo;
     }
 
